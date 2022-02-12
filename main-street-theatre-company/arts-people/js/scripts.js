4a1356ad925598618caa8999a50a71229988c02d
Stephen Kane <steve@mortoneer.com>
2022-02-12 12:39:29 -0500

    feat: academy attendance is good. no more kids

--- main-street-theatre-company/arts-people/js/scripts.js
+++ main-street-theatre-company/arts-people/js/scripts.js
@@ -28,19 +28,21 @@ if (jQuery) {
     $('#membership_level_table').addClass('table')
 
     // Take over the "Retail" page for the Academy.
-    const $retailHeading = $('h1:contains(Welcome to our online store.)');
-    const academyNotStarted = Date.now() < new Date('Feb 15 2022').getTime();
-    if (academyNotStarted && $retailHeading.length) {
-      $retailHeading
-      .text("Main Street Academy - Payment Center")
-      .after(`<p>Please start by reviewing the 
-      <a href="https://www.mainstreettheatrecompany.org/main-street-academy">Main Street Academy page</a>
-       and fill out the <a href="https://docs.google.com/forms/d/e/1FAIpQLScnJYxaupZtM5BzvCt7UWLHaaCMXZtyILsOrqo6pMjSd_pVrA/viewform">form</a> 
-       if you have not done so already.</p>`);
-
-      $('title').text('Main Street Academy - Payment Center');
+    if ($('.academy-take-over').length) {
+      const $retailHeading = $('h1:contains(Welcome to our online store.)');
+      const academyNotStarted = Date.now() < new Date('Feb 15 2022').getTime();
+      if (academyNotStarted && $retailHeading.length) {
+        $retailHeading
+        .text("Main Street Academy - Payment Center")
+        .after(`<p>Please start by reviewing the 
+        <a href="https://www.mainstreettheatrecompany.org/main-street-academy">Main Street Academy page</a>
+         and fill out the <a href="https://docs.google.com/forms/d/e/1FAIpQLScnJYxaupZtM5BzvCt7UWLHaaCMXZtyILsOrqo6pMjSd_pVrA/viewform">form</a> 
+         if you have not done so already.</p>`);
+  
+        $('title').text('Main Street Academy - Payment Center');
+      }
     }
-    
+
     const ATTR_NAME = "data-mortoneer-html-load";
     function loadHtml(index, element) {
       const $form = $(element);
